d1805c4d96e473e54e4485923d9900b7b64fade4
kennpin1 <seiji.kitamura@gmail.com>
2013-08-04 21:13:58 +0900

    修正 php/dataset.class.php 大幅変更

--- php/dataset.class.php
+++ php/dataset.class.php
@@ -7,7 +7,12 @@
 require_once("php/db.class.php");
 
 class dataset extends DB{
+ public $storeinfo;    //店舗情報
+ public $pageinfo;     //ページ情報
+ public $flg0;         //flg0(主にチラシ番号)
  public $items;        //データを格納
+ public $saleday;      //販売日
+ public $saletype;     //セールタイプ
 
  function __construct(){
   parent::__construct();
@@ -18,161 +23,122 @@ class dataset extends DB{
 // 対象テーブル TB_STORE
 // 抽出方法 TB_STOREのcolnameを$colarrayに渡す
 // ================================================================ //
- protected function getStoreInfo($colarray=null){
+ public function datasetStoreInfo(){
   $this->select="colname,jpnname,val";
   $this->from =TB_STORE;
-  $this->items["data"]=$this->getArray();
- }//public function getInfo(){
+  $this->storeinfo=$this->getArray();
+  if(! $this->ary) return false;
+ }//protected function datasetStoreInfo(){
  
 // ================================================================ //
-// ページ情報を抽出
+// 全ページ情報を抽出
 // 対象テーブル TB_PAGECONF
-// 抽出方法     ページ名を指定          
 // ================================================================ //
- public function getPage($pagename=null){
-  $this->items=null;
+ public function datasetPageInfo(){
   //attrをゲット
   $this->select ="attr";
   $this->from =TB_PAGECONF;
   $this->group =$this->select;
-  $this->getArray();
-  $attr=$this->ary;
+  $attr=$this->getArray();
+  if(! $this->ary) throw new exception("列情報がありません");
 
-  $this->items=null;
+  $this->pageinfo=null;
   $this->select =" t.pagename";
   foreach($attr as $rowcnt =>$rowdata){
    $this->select.=",max(case when t.attr='".$rowdata["attr"]."' then t.val else '' end ) as `".$rowdata["attr"]."`";
   }//foreach
   $this->from =TB_PAGECONF." as t ";
-  if($pagename) $this->where=" t.pagename='".$pagename."'";
   $this->group=" t.pagename";
   $this->order =" case when t.attr='parent' then t.val else 9999 end";
   $this->order.=",case when t.attr='group'  then t.val else 9999 end";
   $this->getArray();
-  $this->items["data"]=$this->ary;
- }//public function getpage($pagename){
-
-// ================================================================ //
-// ページグループを抽出
-// 対象テーブル TB_PAGECONF
-// 抽出方法     グループを指定          
-// ================================================================ //
- public function getGroupList($parent){
-  $this->getPage();
-  if(! $this->items["data"]) throw new exception("ページ情報がありません");
-  $data=$this->items["data"];
-  $this->items=null;
-
-  foreach($data as $rows=>$row){
-   if($row["parent"]==$parent || $row["parent"]===0){
-    $this->items["data"][]=$row;
-   }//if
-  }//foraech
- }//public function getGroupList($group=null){
+  $this->pageinfo=$this->ary;
+  if(! $this->ary) return false;
+ }//public function datasetPageInfo(){
 
 // ================================================================ //
 // セールデータを抽出
 // 対象テーブル TB_SALEITEMS
-// 抽出方法     日付を指定          
+// 抽出方法     日付、セールタイプが指定可能        
 // ================================================================ //
- public function getSaleItem($hiduke=null,$saletype=0){
-  if(! $hiduke) $hiduke=date("Y-m-d");
-  if(! is_numeric($saletype)) throw new exception("セールタイプを確認してください");
+ public function datasetSaleItem(){
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+  if($this->saletype && ! is_numeric($this->saletype)) throw new exception("セールタイプを確認してください");
+
   //TB_SALEITEMSデータを抽出
   $this->select="t.*";
   $this->from =TB_SALEITEMS." as t ";
-  $this->where =" t.saleday='".$hiduke."'";
-  if($saletype) $this->where.=" and t.saletype=".$saletype;
+  $this->where =" t.saleday='".$this->saleday."'";
+  $this->where.=" and t.saletype=".$this->saletype;
   $this->order =" t.saletype,t.flg0,t.flg1,t.flg4";
-  $this->items["data"]=$this->getArray();
+  $this->items=$this->getArray();
+  if(! $this->ary) return false;
 
  }//public function getTopMenuData(){
-
-// ================================================================ //
-// メニュー用データを抽出
-// 対象テーブル TB_SALEITEMS
-// 抽出方法     日付を指定 (saletypeごとに1アイテム抽出）         
-// ================================================================ //
- public function getMenuData(){
-  $this->getSaleItem();
-  $saletype=null;
-  foreach($this->items["data"] as $rows=>$row){
-   if($row["saletype"]!=$saletype){
-    $ary[]=$row;
-    $saletype=$row["saletype"];
-   }//if
-  }//foreach
-
-  $this->items["data"]=$ary;
- }// public function getMenuData(){
-
- public function getChildren($pagename){
-  //ページ情報をゲット
-  $this->getPage();
-
-  //$pagenameのページ番号をゲット
-  foreach($this->items["data"] as $rows=>$row){
-   if($row["pagename"]==$pagename){
-    $page=$row["page"];
-    break;
-   }//if
-  }//foreach
-
-  //parentが$pagenameになっているページを抽出
-  foreach($this->items["data"] as $rows=>$row){
-   if($row["parent"]==$page){
-    $ary[]=$row;
-   }
-  }//foreach
-  $this->items=null;
-  $this->items["data"]=$ary;
-
- }//public function getChildren($pagename){
 
 // ================================================================ //
 // ニュースリリース用データ抽出
 // 対象テーブル TB_SALEITEMS
 // ================================================================ //
- public function getNewsData(){
-  $hiduke=date("Y-m-d");
-  $saletype=7;
+ public function datasetNewsData(){
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+  if(! $this->saletype) throw new exception("セールタイプを選択してください");
 
   //TB_SALEITEMSデータを抽出
   $this->select =" t.saleday,t.notice,t.flg8";
   $this->from =TB_SALEITEMS." as t ";
-  $this->where =" t.saleday<='".$hiduke."'";
-  if($saletype) $this->where.=" and t.saletype=".$saletype;
+  $this->where =" t.saleday<='".$this->saleday."'";
+  $this->where.=" and t.saletype=".$this->saletype;
   $this->order =" t.saletype desc";
-  $this->items["data"]=$this->getArray();
+  $this->items=$this->getArray();
+  if(! $this->ary) return false;
  }//public function getNewsData(){
 
 // ================================================================ //
-// チラシ日程データ抽出
+// flg0抽出
 // 対象テーブル TB_SALEITEMS
+// 抽出方法     日付選択可能
 // ================================================================ //
- public function getTirasiDayListData($saleday=null){
-  if(! $saleday) $saleday=date("Y-m-d");
-  if(! ISDATE($saleday)) throw new exception("日付を確認してください");
+ public function datasetGetFlg0(){
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+  if(! $this->saletype) throw new exception("セールタイプを選択してください");
 
   //本日のデータを検索
   $this->select="*";
   $this->from =TB_SALEITEMS;
-  $this->where =" saleday='".$saleday."'";
-  $this->where.=" and saletype=1";
-  $this->items["data"]=$this->getArray();
+  $this->where =" saleday='".$this->saleday."'";
+  $this->where.=" and saletype=".$this->saletype;
+  $this->items=$this->getArray();
+  if(! $this->ary) return false;
 
-  if(! $this->items["data"]) return false;
-  
-  $tirasiid=$this->items["data"][0]["flg0"];
+  $this->flg0=$this->items[0]["flg0"];
+ }//public function datasetGetFlg0(){
+
+// ================================================================ //
+// 日程データ抽出
+// 対象テーブル TB_SALEITEMS
+// ================================================================ //
+ public function datasetDayListData(){
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+
+  if(! $this->saletype) throw new exception("セールタイプを選択してください");
+
+  //本日のデータを検索
+  if(! $this->datasetGetFlg0()) return false;
 
   //チラシの掲載日をゲット
   $this->select="saleday,count(jcode) as jcode";
   $this->from =TB_SALEITEMS;
-  $this->where =" saletype=1";
-  $this->where.=" and flg0='".$tirasiid."'";
+  $this->where =" saletype=".$this->saletype;
+  $this->where.=" and flg0='".$this->flg0."'";
   $this->group ="saleday";
   $this->order ="saleday";
-  $this->items["data"]=$this->getArray();
+  $this->items=$this->getArray();
+  if(! $this->ary) return false;
  }//public function getTirasiDayListData(){
 
 // ================================================================ //
@@ -184,19 +150,13 @@ class dataset extends DB{
 // 表示順 4.クラスコード
 // 表示順 5.JANコード
 // ================================================================ //
- public function getTirasiTanpinListData($saleday=null){
+ public function datasetTanpinListData(){
   //日付確認
-  if(! $saleday) $saleday=date("Y-m-d");
-  if(! ISDATE($saleday)) throw new exception("日付を確認してください");
-  
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+  if(! $this->saletype) throw new exception("セールタイプを選択してください");
   //チラシ番号ゲット
-  $this->select="*";
-  $this->from =TB_SALEITEMS;
-  $this->where =" saleday='".$saleday."'";
-  $this->where.=" and saletype=1";
-  $this->items["data"]=$this->getArray();
-  if(! $this->items["data"]) return false;
-  $tirasiid=$this->items["data"][0]["flg0"];
+  if(! $this->datasetGetFlg0()) return false;
   
   //$saleday以降の商品ゲット
   $this->select =" min(saleday) as salestart";
@@ -205,17 +165,16 @@ class dataset extends DB{
   $this->select.=",flg0,flg1,flg2,flg3,flg4,flg5";
   $this->select.=",flg6,flg7,flg8,flg9";
   $this->from =TB_SALEITEMS;
-  $this->where =" flg0='".$tirasiid."'";
-  $this->where.=" and saleday>='".$saleday."'";
+  $this->where =" flg0='".$this->flg0."'";
+  $this->where.=" and saleday>='".$this->saleday."'";
   $this->group =" clscode,jcode,maker,sname,tani,price,notice";
   $this->group.=",flg0,flg1,flg2,flg3,flg4,flg5";
   $this->group.=",flg6,flg7,flg8,flg9";
-  $this->having=" min(saleday)<='".$saleday."'";
+  $this->having=" min(saleday)<='".$this->saleday."'";
   $this->order =" cast(flg1 as SIGNED)";
   $this->order.=",min(saleday),flg4,clscode,jcode";
-  $this->getArray();
+  $this->items["data"]=$this->getArray();
   if(! $this->ary) return false;
-  $this->items["data"]=$this->ary;
- }//public function getTirasiTanpinListData($saleday=null){
+ }//public function datasetTanpinListData($saleday=null){
 }//class dataset extends DB{
 ?>
