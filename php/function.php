e3584a48861cea10b75a98f06f3d9d221244871d
kennpin1 <seiji.kitamura@gmail.com>
2013-08-07 19:12:46 +0900

    追加修正 php/function.php JPNDATE追加

--- php/function.php
+++ php/function.php
@@ -1,4 +1,24 @@
 <?php
+function is_mobile () {
+ $useragents = array(
+ 'iPhone', // Apple iPhone
+ 'iPod', // Apple iPod touch
+ 'Android', // 1.5+ Android
+ 'dream', // Pre 1.5 Android
+ 'CUPCAKE', // 1.5+ Android
+ 'blackberry9500', // Storm
+ 'blackberry9530', // Storm
+ 'blackberry9520', // Storm v2
+ 'blackberry9550', // Storm v2
+ 'blackberry9800', // Torch
+ 'webOS', // Palm Pre Experimental
+ 'incognito', // Other iPhone browser
+ 'webmate' // Other iPhone browser
+ );
+ $pattern = '/'.implode('|', $useragents).'/i';
+ return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
+}
+
 //-------------------------------------------//
 // 日付チェック                              //
 //-------------------------------------------//
@@ -11,6 +31,19 @@ function ISDATE($hiduke){
     date("m",$moto)!=$match[2] ||
     date("d",$moto)!=$match[3]) return false;
  return true;
+}
+
+//-------------------------------------------//
+// 日付生成                                  //
+//-------------------------------------------//
+function JPNDATE($hiduke){
+ global $YOUBI;
+ if(! ISDATE($hiduke)) return false;
+ $unixtime=strtotime($hiduke);
+ $d=date("Y年m月d日",$unixtime);
+ $w=date("w",$unixtime);
+ $y=$YOUBI[$w];
+ return $d."(".$y.")";
 }
 
 //-------------------------------------------//
