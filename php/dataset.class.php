fb00ba26afcddc09497eb2ba4034da51fd297df1
kennpin1 <seiji.kitamura@gmail.com>
2013-08-07 19:12:14 +0900

    追加修正 php/dataset.class.php チラシ系データ抽出

--- php/dataset.class.php
+++ php/dataset.class.php
@@ -13,6 +13,9 @@ class dataset extends DB{
  public $items;        //データを格納
  public $saleday;      //販売日
  public $saletype;     //セールタイプ
+ public $lincode;      //ラインコード
+ public $clscode;      //ラインコード
+ public $jcode;      //ラインコード
 
  function __construct(){
   parent::__construct();
@@ -28,6 +31,7 @@ class dataset extends DB{
   $this->from =TB_STORE;
   $this->storeinfo=$this->getArray();
   if(! $this->ary) return false;
+  return true;
  }//protected function datasetStoreInfo(){
  
 // ================================================================ //
@@ -54,7 +58,76 @@ class dataset extends DB{
   $this->getArray();
   $this->pageinfo=$this->ary;
   if(! $this->ary) return false;
+  return true;
  }//public function datasetPageInfo(){
+
+// ================================================================ //
+// ページ情報を抽出
+// 対象テーブル TB_PAGECONF
+// ================================================================ //
+  public function datasetPageData($pagename){
+   if(! $this->datasetPageInfo()) return false;
+   $ary="";
+
+   foreach($this->pageinfo as $rows=>$row){
+    if($row["pagename"]==$pagename){
+     $ary[]=$this->pageinfo[$rows];
+     break;
+    }//if
+   }//foreach
+   if(! $ary) return false;
+   $this->pageinfo=$ary;
+   return true;
+  }// public function datasetPageData($pagename){
+
+// ================================================================ //
+// グループ情報を抽出(兄弟)
+// 対象テーブル TB_PAGECONF
+// ================================================================ //
+  public function datasetBroth($pagename){
+   if(! $this->datasetPageInfo()) return false;
+   $ary="";
+   
+   //$pagenameのparentをゲット
+   foreach($this->pageinfo as $rows=>$row){
+    if($row["pagename"]==$pagename){
+     $parent=$row["parent"];
+     break;
+    }//if
+   }//foreach
+
+   //同じparentを持つページをゲット
+   foreach($this->pageinfo as $rows=>$row){
+    if($row["parent"]==$parent){
+     $ary[]=$this->pageinfo[$rows];
+    }
+   }//foreach
+
+   if(! $ary) return false;
+   $this->pageinfo=$ary;
+   return true;
+  }//public function datasetBroth($parent){
+
+
+// ================================================================ //
+// グループ情報を抽出(子供)
+// 対象テーブル TB_PAGECONF
+// ================================================================ //
+  public function datasetChildren($parent){
+   if(! $this->datasetPageInfo()) return false;
+   $ary="";
+
+   foreach($this->pageinfo as $rows=>$row){
+    if($row["parent"]==$parent || $row["parent"]==="0"){
+     $ary[]=$this->pageinfo[$rows];
+    }//if
+   }//foreach
+
+   if(! $ary) return false;
+   $this->pageinfo=$ary;
+   return true;
+
+  }//public function datasetChildren($parent){
 
 // ================================================================ //
 // セールデータを抽出
@@ -97,7 +170,7 @@ class dataset extends DB{
  }//public function getNewsData(){
 
 // ================================================================ //
-// flg0抽出
+// flg0抽出(チラシ番号)
 // 対象テーブル TB_SALEITEMS
 // 抽出方法     日付選択可能
 // ================================================================ //
@@ -105,7 +178,6 @@ class dataset extends DB{
   if(! $this->saleday) $this->saleday=date("Y-m-d");
   if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
   if(! $this->saletype) throw new exception("セールタイプを選択してください");
-
   //本日のデータを検索
   $this->select="*";
   $this->from =TB_SALEITEMS;
@@ -115,6 +187,7 @@ class dataset extends DB{
   if(! $this->ary) return false;
 
   $this->flg0=$this->items[0]["flg0"];
+  return true;
  }//public function datasetGetFlg0(){
 
 // ================================================================ //
@@ -124,13 +197,12 @@ class dataset extends DB{
  public function datasetDayListData(){
   if(! $this->saleday) $this->saleday=date("Y-m-d");
   if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
-
   if(! $this->saletype) throw new exception("セールタイプを選択してください");
 
   //本日のデータを検索
   if(! $this->datasetGetFlg0()) return false;
 
-  //チラシの掲載日をゲット
+  //期間中の日程をゲット
   $this->select="saleday,count(jcode) as jcode";
   $this->from =TB_SALEITEMS;
   $this->where =" saletype=".$this->saletype;
@@ -139,8 +211,37 @@ class dataset extends DB{
   $this->order ="saleday";
   $this->items=$this->getArray();
   if(! $this->ary) return false;
+  return true;
  }//public function getTirasiDayListData(){
 
+// ================================================================ //
+// lincodeグループデータ抽出
+// 対象テーブル TB_SALEITEMS
+// ================================================================ //
+ public function datasetLinGroup(){
+  if(! $this->saleday) $this->saleday=date("Y-m-d");
+  if(! ISDATE($this->saleday)) throw new exception("日付を確認してください");
+  if(! $this->saletype) throw new exception("セールタイプを選択してください");
+  //本日のデータを検索
+  if(! $this->datasetGetFlg0()) return false;
+
+  //期間中のlincodeをゲット
+  $this->select=" t2.lincode,t2.linname,count(t.jcode) as jcode";
+  $this->from =TB_SALEITEMS." as t ";
+  $this->from.=" inner join ".TB_CLSMAS." as t1 on";
+  $this->from.=" t.clscode=t1.clscode";
+  $this->from.=" inner join ".TB_LINMAS." as t2 on";
+  $this->from.=" t1.lincode=t2.lincode";
+  $this->where =" t.saletype=".$this->saletype;
+  $this->where.=" and t.flg0='".$this->flg0."'";
+  $this->where.=" and t.saleday='".$this->saleday."'";
+  $this->group ="t2.lincode,t2.linname";
+  $this->order ="t2.lincode";
+  $this->items=$this->getArray();
+  if(! $this->ary) return false;
+  return true;
+
+ }//public function datasetLinGroup(){
 // ================================================================ //
 // チラシ単品データ抽出
 // 対象テーブル TB_SALEITEMS
@@ -159,22 +260,43 @@ class dataset extends DB{
   if(! $this->datasetGetFlg0()) return false;
   
   //$saleday以降の商品ゲット
-  $this->select =" min(saleday) as salestart";
-  $this->select.=",max(saleday) as saleend,";
-  $this->select.=" clscode,jcode,maker,sname,tani,price,notice";
-  $this->select.=",flg0,flg1,flg2,flg3,flg4,flg5";
-  $this->select.=",flg6,flg7,flg8,flg9";
-  $this->from =TB_SALEITEMS;
-  $this->where =" flg0='".$this->flg0."'";
-  $this->where.=" and saleday>='".$this->saleday."'";
-  $this->group =" clscode,jcode,maker,sname,tani,price,notice";
-  $this->group.=",flg0,flg1,flg2,flg3,flg4,flg5";
-  $this->group.=",flg6,flg7,flg8,flg9";
-  $this->having=" min(saleday)<='".$this->saleday."'";
-  $this->order =" cast(flg1 as SIGNED)";
-  $this->order.=",min(saleday),flg4,clscode,jcode";
-  $this->items["data"]=$this->getArray();
+  $this->select =" min(t.saleday) as salestart";
+  $this->select.=",max(t.saleday) as saleend,";
+  $this->select.=" t.clscode,t.jcode,t.maker,t.sname,t.tani,t.price,t.notice";
+  $this->select.=",t.flg0,t.flg1,t.flg2,t.flg3,t.flg4,t.flg5";
+  $this->select.=",t.flg6,t.flg7,t.flg8,t.flg9";
+  $this->from =TB_SALEITEMS." as t";
+  $this->from.=" inner join ".TB_CLSMAS." as t1 on";
+  $this->from.=" t.clscode=t1.clscode";
+  $this->where =" t.flg0='".$this->flg0."'";
+  $this->where.=" and t.saleday>='".$this->saleday."'";
+  if($this->lincode) $this->where.=" and t1.lincode=".$this->lincode;
+  $this->group.=" t.clscode,t.jcode,t.maker,t.sname,t.tani,t.price,t.notice";
+  $this->group.=",t.flg0,t.flg1,t.flg2,t.flg3,t.flg4,t.flg5";
+  $this->group.=",t.flg6,t.flg7,t.flg8,t.flg9";
+  $this->having=" min(t.saleday)<='".$this->saleday."'";
+  $this->order =" cast(t.flg1 as SIGNED)";
+  $this->order.=",min(t.saleday),t.flg4,t.clscode,t.jcode";
+  $this->items=$this->getArray();
   if(! $this->ary) return false;
+  return true;
  }//public function datasetTanpinListData($saleday=null){
+ 
+// ================================================================ //
+// クラスデータ抽出(lincode編)
+// 対象テーブル TB_CLSMAS
+// 抽出方法 $this->lincodeに属するclscode
+// ================================================================ //
+ public function datasetClsList(){
+  $this->select="*";
+  $this->from =TB_CLSMAS;
+  if($this->lincode) $this->where="lincode=".$this->lincode;
+  $this->order ="clscode";
+  $this->getArray();
+  if(! $this->ary) return false;
+  $this->items=$this->ary;
+  return true;
+ }// public function datasetClsList(){
+
 }//class dataset extends DB{
 ?>
