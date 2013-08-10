bb241304f2b2f471261f652c9b6dcd0b0a2f73c2
kennpin1 <seiji.kitamura@gmail.com>
2013-08-11 06:55:22 +0900

    追加 tirasitanpini.php 単品表示用ページ作成

---
+++ tirasitanpin.php
@@ -0,0 +1,38 @@
+<?php
+require_once("php/page.class.php");
+try{
+ $saleday=$_GET["saleday"];
+ if(! $saleday) $saleday=date("Y-m-d");
+ if(! ISDATE($saleday)) throw new exception("日付を確認してください");
+
+ $jcode=$_GET["jcode"];
+ if(! CHKCD($jcode)) throw new exception("JANコードを確認してください");
+
+ $db=new page();
+ $db->me=basename($_SERVER["PHP_SELF"]);
+ $db->saletype=1;
+ $db->saleday=$saleday;
+ $db->jcode=$jcode;
+
+ //フレームをセット
+ $db->pageFram();
+ 
+ //ヘッダーをセット
+ $db->part=$db->pageHeader();
+ $db->appendhtml("header");
+
+ //単品データゲット
+ $db->pageTanpin();
+
+ //leftside
+ $db->me="tirasidaylist.php";
+ $db->pageTirasiDayListLeftSide();
+ $db->pageTirasiLinListLeftSide();
+  
+ echo $db->html;
+
+}//try
+catch(Exception $e){
+ echo "エラー:".$e->getMessage();
+}
+?>
